78d2e17f9a777a96a14d1799082aff65c23ac22a
A77-web <83652921+A77-web@users.noreply.github.com>
2022-06-18 21:53:53 +0800

    归并排序

---
+++ 0618.cpp
@@ -0,0 +1,35 @@
+void _MergeSort(int ar[], int left, int right, int* tmp)
+{
+	//分解
+	if (left >= right)
+		return;
+	int mid = (left + right) / 2;
+	_MergeSort(ar, left, mid, tmp);
+	_MergeSort(ar, mid + 1, right, tmp);
+	//合并
+	int begin1, end1, begin2, end2;
+	begin1 = left, end1 = mid;
+	begin2 = mid + 1, end2 = right;
+	int k = left;
+	while (begin1 <= end1 && begin2 <= end2)
+	{
+		if (ar[begin1] < ar[begin2])
+			tmp[k++] = ar[begin1++];
+		else
+			tmp[k++] = ar[begin2++];
+	}
+	while (begin1 <= end1)
+		tmp[k++] = ar[begin1++];
+	while (begin2 <= end2)
+		tmp[k++] = ar[begin2++];
+	memcpy(ar + left, tmp + left, sizeof(int) * (right - left + 1));
+
+}
+void MergeSort(int ar[], int left, int right)
+{
+	int n = right - left;
+	int* tmp = (int*)malloc(sizeof(int) * n);
+	_MergeSort(ar, left, right - 1, tmp);
+	free(tmp);
+
+}
